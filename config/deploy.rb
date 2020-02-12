3475ed7f4a31b11549837f1708b092aabf9eb1b7
hayashinaotoo <hysnot1005@yahoo.co.jp>
2020-02-12 17:02:12 +0900

    modify-file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_68c.pem'
+set :application, 'freemarket_sample_68c'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:keita2630401/freemarket_sample_68c.git'
