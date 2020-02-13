693b397608fcbdc7ef7a9197aef3eddd3141577e
hayashinaotoo <hysnot1005@yahoo.co.jp>
2020-02-13 11:45:15 +0900

    changed-s3-saved-place

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,16 @@ include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  process resize_to_fit: [300, 300]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -29,7 +39,7 @@ include CarrierWave::MiniMagick
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [50, 50]
+    # process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
