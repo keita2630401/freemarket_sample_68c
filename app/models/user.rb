29882e237fd74691a9d7ab8c88e5a531e9d0770d
sotaro0309 <56988972+sotaro0309@users.noreply.github.com>
2020-02-13 13:32:45 +0900

    user_model_create
    Userモデルを作りました。
    パスワードは半角英数字を絡め７文字以上出ないと、登録できないようになっております。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :nickname, presence: true
+  validates :last_name,         presence: true
+  validates :first_name,        presence: true
+  validates :last_name_kana,    presence: true
+  validates :first_name_kana,   presence: true
+  validates :nickname,          presence: true
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX}
+  validates :encrypted_password, length: { minimum: 7 }
 end
