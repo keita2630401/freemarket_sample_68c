ad490cdcfd5efe5513a210a8bc4bfb2f3a40e7bb
ono-max <nono19@students.desu.edu>
2020-02-13 12:40:42 +0900

    add migrate

--- db/migrate/20200213025956_create_items.rb
+++ db/migrate/20200213025956_create_items.rb
@@ -6,6 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :shipping_cost_side, null: false
       t.string  :origin_area,       null: false
       t.string  :shipping_days,     null: false
+      t.integer :price,             null: false
       t.timestamps
     end
   end
