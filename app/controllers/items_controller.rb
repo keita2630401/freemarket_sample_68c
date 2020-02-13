44c0f20f647b165cf0e0b51c3d382b9365d45a56
ono-max <nono19@students.desu.edu>
2020-02-13 14:10:52 +0900

    create controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,21 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
+    @item.images.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :shipping_cost_side, :origin_area, :shipping_days, :price,  :image_attributes: [:src])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :images, :dependent => :delete_all
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
