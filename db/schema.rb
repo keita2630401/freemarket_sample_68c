3e144ab389a2dfaaf6a5474bbadb8b3111256bb1
sotaro0309 <56988972+sotaro0309@users.noreply.github.com>
2020-02-13 17:15:42 +0900

    adress_table create
    adressテーブルを作成しました。
    usersテーブルとアソシエーションしています。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_025021) do
+ActiveRecord::Schema.define(version: 2020_02_13_080649) do
+
+  create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "post_number", null: false
+    t.string "prefecture", null: false
+    t.string "municipality", null: false
+    t.string "town", null: false
+    t.string "building", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
