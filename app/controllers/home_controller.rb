6ffdd167eab7fe17f4ecc80e1fdb2e9d86f2b4c2
sotaro0309 <56988972+sotaro0309@users.noreply.github.com>
2020-02-17 13:13:09 +0900

    commit fix
    masterの状況を確認したところ、ルーティングが変更されているようでしたので記述に変更がございます。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def top
-    @items = Item.where(status: 'exihibiting').includes(:user).order("created_at DESC").page(params[:page]).per(3)
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.where(status: 'exihibiting').includes(:user).order("created_at DESC").page(params[:page]).per(3)
+  end
+
   def new
     @item = Item.new
     @item.images.new
