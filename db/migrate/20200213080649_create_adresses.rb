704b8f6106d5b980cd4b1b5aad37524f795acae0
sotaro0309 <56988972+sotaro0309@users.noreply.github.com>
2020-02-17 16:54:05 +0900

    adress_table edit
    送付先氏名と任意で電話番号を登録できるようになりました。
    電話番号の登録は任意の為、validatesは設定していません。

--- db/migrate/20200213080649_create_adresses.rb
+++ db/migrate/20200213080649_create_adresses.rb
@@ -1,10 +1,15 @@
 class CreateAdresses < ActiveRecord::Migration[5.2]
   def change
     create_table :adresses do |t|
+      t.string  :family_name,      null: false
+      t.string  :first_name,       null: false
+      t.string  :family_name_kana, null: false
+      t.string  :first_name_kana,  null:false
       t.integer :post_number,      null: false
-      t.string  :prefecture,       null:false
-      t.string  :municipality,     null:false
-      t.string  :town,             null:false
+      t.string  :prefecture,       null: false
+      t.string  :municipality,     null: false
+      t.string  :town,             null: false
+      t.integer :telephone_number,  
       t.integer :user_id,          null:false, foreign_key:true
 
       t.timestamps
