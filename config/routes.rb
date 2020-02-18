5a3542cce4ed43904912b4dc407f30b5fe0abab8
masachan-100 <59827466+masachan-100@users.noreply.github.com>
2020-02-18 14:29:00 +0900

    adress_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
   root  'items#index'
   resources :adresses, only: [:index, :new, :create, :edit, :update]
   resources :items, only: [:new, :create]
