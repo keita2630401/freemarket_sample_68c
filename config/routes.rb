674bfe1f9a5383824f43d50983c842058ce82b92
sotaro0309 <56988972+sotaro0309@users.noreply.github.com>
2020-02-20 15:35:56 +0900

    mypage-routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root  'items#index'
   resources :adresses, only: [:index, :new, :create, :edit, :update]
   resources :items, only: [:new, :create]
+  resources :users, only: [:index]
+  get 'users/logout', to: 'users#logout'
+  get 'users/card',   to: 'users#card'
+  get 'users/create', to: 'users#create'
 end
