6c7978e084a009480fe4483810310cab17a73d59
sotaro0309 <56988972+sotaro0309@users.noreply.github.com>
2020-02-20 15:36:32 +0900

    mypage-javascript

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,46 @@
+$(function(){
+  $(".MyPage__linkbox").hover(function() {
+    $(this).css("background-color", "#EEEEEE");
+    $(this).css("color", "#333333");
+    $(".MyPage__linkbox__right", this).css("font-size", "22px");
+    $(".MyPage__linkbox__right", this).css("padding-left", "25px");
+  }, function() {
+    $(this).css("background-color", "white");
+    $(this).css("color", "#666666");
+    $(".MyPage__linkbox__right", this, ".MyPage__linkbox__right").css("font-size", "17px");
+    $(".MyPage__linkbox__right", this).css("padding-left", "20px");
+  });
+
+
+  $(".MyPage__box__right__box__bottom--product__log--left").hover(function() {
+    $(this).css("color", "#666666");
+  }, function(){
+    $(this).css("color", "#444444");
+  });
+  $(".MyPage__box__right__box__bottom--product__log--right").hover(function() {
+    $(this).css("color", "#666666");
+  }, function(){
+    $(this).css("color", "#444444");
+  });
+
+
+  $(document).on('click', '.MyPage__box__right__box__bottom--product__log--right', function() {
+    $(this).css("border-top", "2px solid red");
+    $(this).css("background-color", "white");
+    $(".MyPage__box__right__box__bottom--product__log--left").css("background-color", "#eeeeee");
+    $(".MyPage__box__right__box__bottom--product__log--left").css("border-top", "0px");
+  }); 
+  $(document).on('click', '.MyPage__box__right__box__bottom--product__log--left', function() {
+    $(this).css("border-top", "2px solid red");
+    $(this).css("background-color", "white");
+    $(".MyPage__box__right__box__bottom--product__log--right").css("background-color", "#eeeeee");
+    $(".MyPage__box__right__box__bottom--product__log--right").css("border-top", "0px");
+  }); 
+
+
+  $("#add_card_btn").hover(function() {
+    $(this).css("background-color", "#FF5555");
+  }, function(){
+    $(this).css("background-color", "#FF3333");
+  });
+});
