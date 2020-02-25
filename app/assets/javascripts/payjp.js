f448da06d54ab5b3386cb80f72445b79cb7aef00
masachan-100 <59827466+masachan-100@users.noreply.github.com>
2020-02-25 14:43:19 +0900

    商品購入機能
    カードが登録できない不具合を修正しました。

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,32 +1,30 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
-    if ($("#token_submit") != null) {
-      Payjp.setPublicKey("pk_test_d6add0a50dea2eaa6e89d3d8");
-      $("#token_submit").on("click",function() {
-        e.preventDefault();
-        let card = {
-          number: $("#card_number").val(),
-          cvc: $("#cvc").val(),
-          exp_month: $("#exp_month").val(),
-          exp_year: $("#exp_year").val()
+    Payjp.setPublicKey("pk_test_d6add0a50dea2eaa6e89d3d8");
+    $("#token_submit").on("click",function(e) {
+      e.preventDefault();
+      let card = {
+        number: $("#card_number").val(),
+        cvc: $("#cvc").val(),
+        exp_month: $("#exp_month").val(),
+        exp_year: $("#exp_year").val()
+      };
+
+      Payjp.createToken(card, (status, response) => {
+        if (status === 200) {
+          $("#card_number").removeAttr("name");
+          $("#cvc").removeAttr("name");
+          $("#exp_month").removeAttr("name");
+          $("#exp_year").removeAttr("name");
+          $("#card_token").append(
+            $('<input type="hidden" name="payjp-token">').val(response.id)
+          );
+          document.inputForm.submit();
+          alert("登録が完了しました");
+        } else {
+          alert("カード情報が正しくありません。");
         }
-        Payjp.createToken(card, (status, response) => {
-          if (status === 200) {
-            $("#card_number").removeAttr("name");
-            $("#cvc").removeAttr("name");
-            $("#exp_month").removeAttr("name");
-            $("#exp_year").removeAttr("name");
-            $("#card_token").append(
-              $('<input type="hidden" name="payjp-token">').val(response.id)
-            );
-            document.inputForm.submit();
-            alert("登録が完了しました");
-          } else {
-            alert("カード情報が正しくありません。");
-          }
-        });
       });
-    }
-  },
-  false
-);
+    });
+  }
+)
