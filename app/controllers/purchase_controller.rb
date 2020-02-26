f1b4b0dfd9eebc6f777d81e99a78cddb806e1e67
masachan-100 <59827466+masachan-100@users.noreply.github.com>
2020-02-26 16:20:38 +0900

    ビューの修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -7,12 +7,12 @@ class PurchaseController < ApplicationController
     if @card.blank?
       redirect_to controller: "cards", action: "new"
     else
+      @item = Item.find(params[:item_id])
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
-
 
   def pay
     @item = Item.find(params[:item_id])
@@ -24,6 +24,10 @@ class PurchaseController < ApplicationController
     )
     redirect_to action: 'done'
     @item.update(status:'購入済み')
+  end
+
+  def done
+    @item = Item.find(params[:item_id])
   end
 
   private
