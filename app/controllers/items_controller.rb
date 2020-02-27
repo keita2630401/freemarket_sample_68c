58f69ea06c0d6083c2d830b5917ea2200ad8e66c
hayashinaotoo <hysnot1005@yahoo.co.jp>
2020-02-27 20:21:27 +0900

    modify-itemsEditPage-selecting-categories

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,17 +2,18 @@ class ItemsController < ApplicationController
   require 'payjp'
   before_action :set_item, only: [:show, :destroy, :edit, :update]
   before_action :set_category, only: [:index, :show]
-  
+  before_action :set_category_edit, only: [:edit, :update]
+
   def index
     @itemsExihibiting = Item.where(status: :exihibiting).last(3).reverse
   end
-  
+
   def show
     @items = Item.all
     @card = Card.where(user_id: current_user.id).first
     @category = Category.find(@item.category_id)
   end
-  
+
   def new
     @item = Item.new
     @item.images.new
@@ -23,7 +24,7 @@ class ItemsController < ApplicationController
       format.json { @children }
     end
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -32,7 +33,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def destroy
     if @item.destroy
       redirect_to root_path
@@ -40,13 +41,13 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     end
   end
-  
+
   def purchase
   end
-  
+
   def edit
   end
-  
+
   def update
     if @item.update(item_params)
       redirect_to root_path
@@ -54,20 +55,28 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  
+
   def purchase
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :condition_id, :shipping_cost_side_id, :prefecture_id, :shipping_days_id, :price, :category_id, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
   end
-  
+
   def set_item
     @item = Item.find(params[:id])
   end
-  
+
   def set_category
     @parents = Category.roots
+  end
+
+  def set_category_edit
+    @grandChildCategory = Category.find(@item.category_id)
+    @childCategory = @grandChildCategory.parent
+    @parent = @childCategory.parent
+    @grandChildCategories = @childCategory.children
+    @childCategories = @parent.children
   end
 end
